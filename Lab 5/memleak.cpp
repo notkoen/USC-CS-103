91b8f0cd31957448f18d18ad1f25ac335513ebc2
darfrey-dev <45914779+darfrey-dev@users.noreply.github.com>
2022-03-11 12:49:38 -0800

    Add Final Submissions

--- Lab 5/memleak.cpp
+++ Lab 5/memleak.cpp
@@ -1,76 +1,97 @@
+/*
+ * Name: Aaron Kuo
+ * Class: USC CSCI 103
+ * Assignment: CSCI 103L Lab 5 (Memleak.cpp)
+ * Additonal Info: Coded in VSC and debugged with GCC
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
 using namespace std;
 
-
-// sum an array of numbers
+//Calculate the sum of an array
 int test1(int *data, int len)
 {
-  int sum = 0;
-  for(int i=0; i <= len; i++){
-    sum += data[i];
-  }
-  return sum;
+    int sum = 0;
+    //for(int i = 0; i <= len; i++)
+    //i <= len will result in array out of bounds issue as arrays start at index 0
+    //Having = will result in accessing past the length of array
+    for(int i = 0; i < len; i++)
+    {
+        sum += data[i];
+    }
+    return sum;
 }
 
-// Allocate a random number array
+//Allocate a random number array
 char* test2_init(int len)
 {
-  char buf[80];
-  cout << "Enter a word: ";
-  cin >> buf;
-  char* mydat = new char[strlen(buf)+1];
-  strcpy(mydat, buf);
-  return mydat;
+    char buf[80];
+    cout << "Enter a word: ";
+    cin >> buf;
+    char* mydat = new char[strlen(buf)+1];
+    strcpy(mydat, buf);
+    return mydat;
 }
 
 char* test2_reverse(char* word)
 {
-  int len = strlen(word);
-  char* otherword = new char[len+1];
-  
-  for(int i=0; i < strlen(word); i++){
-    otherword[i] = word[len-i-1];
-  }
-  otherword[len+1] = '\0';
-  delete [] word;
-  return otherword;
+    int len = strlen(word);
+    char* otherword = new char[len+1];
+    
+    for(int i=0; i < strlen(word); i++)
+    {
+        otherword[i] = word[len-i-1];
+    }
+    //otherword[len+1] = '\0';
+    //Using len + 1 goes beyond the array
+    otherword[len] = '\0';
+    //delete [] word;
+    //Commented out since main already does this
+    return otherword;
 }
 
 int main(int argc, char* argv[])
 {
-  if(argc < 2){
-    cerr << "Please enter the test number you want to run [1-2]" << endl;
-    return 1;
-  }
-  const int len = 7;
-  int test = atoi(argv[1]);
-
-  if(test == 1){
-    // Test should sum up the array values and print it
-    int *data = new int[len];
-    for(int i=0; i < len; i++){
-      data[i] = rand()%100;
+    if(argc < 2)
+    {
+        cerr << "Please enter the test number you want to run [1-2]" << endl;
+        return 1;
     }
-    int sum = test1(data, len);
-    cout << "Test 1 sum is " << sum << endl;
-  }
+    const int len = 7;
+    int test = atoi(argv[1]);
+    
+    if(test == 1)
+    {
+        // Test should sum up the array values and print it
+        //int *data = new int[len];
+        //No need for a pointer above
+        int data[len];
+        for (int i=0; i < len; i++)
+        {
+            data[i] = rand()%100;
+        }
+        int sum = test1(data, len);
+        cout << "Test 1 sum is " << sum << endl;
+    }
+    
+    else if(test == 2)
+    {
+        // Test should create a random string & then reverse a copy
+        char* myword = test2_init(len);
+        cout << "My word is: " << myword << endl;
+        
+        char* otherword = test2_reverse(myword);
+        cout << "The other word is: " << otherword << endl;
+        
+        delete [] myword;
+        delete [] otherword;
+    }
+    else
+    {
+        cout << "Unknown test number" << endl;
+    }
 
-  else if(test == 2){
-    // Test should create a random string & then reverse a copy
-    char* myword = test2_init(len);
-    cout << "myword is " << myword << endl;
- 
-    char* otherword = test2_reverse(myword);    
-    cout << "otherword is " << otherword << endl;
-   
-    delete [] myword;
-    delete [] otherword;
-  }
-  else {
-    cout << "Unknown test number" << endl;
-  }
-
-  return 0;
+    return 0;
 }
--- Lab 5/scramble.cpp
+++ Lab 5/scramble.cpp
@@ -1,7 +1,7 @@
 /*
  * Name: Aaron Kuo
  * Class: USC CSCI 103
- * Assignment: CSCI 103L Lab 5
+ * Assignment: CSCI 103L Lab 5 (Scramble.cpp)
  * Additonal Info: Coded in VSC and debugged with GCC
  */
 
@@ -13,6 +13,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
+#include <fstream> //For reading files
 
 using namespace std;
 
@@ -21,64 +22,113 @@ void permute(char items[], int len);
 
 /* Delete the array below and replace by reading in words from a file 
    in main() */
-const char* wordBank[] = {
-   "computer", "president", "trojan", "program", "coffee",
-   "library", "football", "popcorn", "science", "engineer"};
-
 const int numWords = 10;
 
-int main() {
-  srand(time(0));
-  char guess[80];
-
-  bool wordGuessed = false;
-  int numTurns = 10;
-
-  /* Add code to declare the wordBank array, read in each word
-     and store it */
+int main(int argc, char* argv[])
+{
+    srand(time(0));
+    char guess[80];
     
+    bool wordGuessed = false;
+    int numTurns = 10;
     
-  // Pick a random word from the wordBank
-  int target = rand() % numWords;
-  int targetLen = strlen(wordBank[target]);
+    int numWords;
+    
+    //Check if input has the correct amount of arguments
+    if(argc < 2)
+    {
+        cout << "Usage: ./scramble <input file>" << endl;
+        return -1;
+    }
+    
+    //Use ifstream to load up file specified by launch params
+    ifstream file(argv[1]);
+    
+    //Check if file exists, if not then warn the user
+    if (file.fail())
+    {
+        cout << "File provided does not exist. Please try again." << endl;
+        file.clear();
+        return -1;
+    }
+    
+    //Check if file specifies number of words in first line, if not then warn the user
+    file >> numWords;
+    if (file.fail())
+    {
+        cout << "Number of words not specified." << endl;
+        return -2;
+    }
+    
+    char** wordBank = new char*[numWords];
+    char w[50];
+    int wlen = 0;
+    
+    //takes in exactly the number of words specified
+    //words separated by whitespace
+    for (int i = 0; i < numWords; i++)
+    {
+        file >> w;
+        wlen = strlen(w);
+        wordBank[i] = new char[wlen+1];
+        strncpy(wordBank[i], w, wlen+1);
+        //cout << w << endl;
+    }
+    
+    //Once we are done, stop reading the file
+    file.close();
+    
+    //Pick a random word from the wordBank
+    int target = rand() % numWords;
+    int targetLen = strlen(wordBank[target]);
 
-  // Make a dynamically-allocated copy of the word and scramble it
-  char* word = new char[targetLen+1];
-  strcpy(word, wordBank[target]);
-  permute(word, targetLen);
-  
-  // An individual game continues until a word
-  //  is guessed correctly or 10 turns have elapsed
-  while (!wordGuessed && numTurns > 0) {
-    cout << "Scrambled word: " << word << endl;
-    cout << "What do you guess the original word is? ";
-    cin >> guess;
-    /* Complete the next line of code */
-      
-    wordGuessed = // ADD code to check if the guess is equal to the chosen word.
-                  // wordGuessed should be true if they are equal, and false otherwise
+    //Make a dynamically-allocated copy of the word and scramble it
+    char* word = new char[targetLen+1];
+    strcpy(word, wordBank[target]);
+    permute(word, targetLen);
+    
+    // An individual game continues until a word
+    //  is guessed correctly or 10 turns have elapsed
+    while (!wordGuessed && numTurns > 0)
+    {
+        cout << "Scrambled word: " << word << endl;
+        cout << "What do you think the unscrambled word is?" << endl;
+        cin >> guess;
         
-    numTurns--;   // Every guess counts as a turn
-  }
-  if (wordGuessed) {
-    cout << "You win!" << endl;
-  }
-  else {
-    cout << "Too many turns...You lose!" << endl;
-  }
-  /* Free up any necessary memory */
+        wordGuessed = (strcmp(wordBank[target],guess) == 0);
+        
+        numTurns--;   // Every guess counts as a turn
+    }
+    if (wordGuessed)
+    {
+        cout << "You win!" << endl;
+    }
+    else
+    {
+        cout << "Too many turns...You lose!" << endl;
+    }
     
-  delete [] word;
-  return 0;
+    //Free up any necessary memory
+    delete [] word;
+    
+    //Delete the arrays within
+    for (int i = 0; i < numWords; i++)
+    {
+        delete [] wordBank[i];
+    }
+    
+    delete [] wordBank;
+    return 0;
 }
 
 // Scramble the letters. See "Knuth shuffle".
-void permute(char items[], int len) {
-  for (int i = len-1; i > 0; --i) {
-    int r = rand() % i;
-    char temp = items[i];
-    items[i] = items[r];
-    items[r] = temp;
-  }
+void permute(char items[], int len)
+{
+    for (int i = len-1; i > 0; --i)
+    {
+        int r = rand() % i;
+        char temp = items[i];
+        items[i] = items[r];
+        items[r] = temp;
+    }
 }
-
